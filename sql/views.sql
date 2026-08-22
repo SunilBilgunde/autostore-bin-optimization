f3fc7ceefc178c6599fb917ccdb36aef098389be
Sunil Bilgunde <sunilbilgunde1996@gmail.com>
2026-08-22 12:28:10 +0100

    Add velocity analysis and slotting recommendation views

--- sql/views.sql
+++ sql/views.sql
@@ -38,3 +38,36 @@ SELECT
     sku AS part_sku,
     quantity
 FROM order_lines;
+
+CREATE VIEW velocity_by_sku AS
+SELECT
+    p.brand,
+    p.category,
+    p.part_sku,
+    COUNT(*) AS pick_events
+FROM fact_order_lines f
+JOIN dim_parts p ON f.part_sku = p.part_sku
+GROUP BY p.brand, p.category, p.part_sku;
+
+CREATE VIEW slotting_recommendation AS
+WITH ranked AS (
+    SELECT
+        part_sku,
+        brand,
+        category,
+        pick_events,
+        NTILE(5) OVER (ORDER BY pick_events DESC) AS velocity_tier
+    FROM velocity_by_sku
+)
+SELECT
+    part_sku,
+    brand,
+    category,
+    pick_events,
+    velocity_tier,
+    CASE
+        WHEN velocity_tier = 1 THEN 'Fast-Access'
+        WHEN velocity_tier IN (2, 3) THEN 'Standard'
+        ELSE 'Deep-Storage'
+    END AS slotting_zone
+FROM ranked;
